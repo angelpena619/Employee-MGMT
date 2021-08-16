91e0f0ceae5d57c337423af388f77a8864e47483
Angel <angelpena619@gmail.com>
2021-08-16 06:27:32 -0700

    adding port connection

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ let roles;
 let departments;
 let managers;
 let employees;
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 8000,
+    user: "root",
+    password: "P@ssw0rd",
+    database: "employees_db"
+  });
+
+  figlet('FSC Employee Tracker', (err, result) => {
+    console.log(err || result);
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+    getDepartments();
+    getRoles();
+    getManagers();
+    getEmployees();
+  });
+
+  start = () => {
